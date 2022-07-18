3a0cb59bb58452d9c1ac4c5e4577124c37c5e964
NickP2791 <admin@roxanick.com>
2022-07-18 17:10:37 -0500

    dotenv change

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
+import dotenv from "dotenv/config";
 import path from "path";
 const __dirname = path.resolve();
-dotenv.config();
+// dotenv.config();
 
 import userRoutes from "./routes/user.js";
 
