52cc0dcf1258eed9d1e95b9528c2e2f82665170a
NickP2791 <admin@roxanick.com>
2022-09-08 10:59:55 -0500

    user log query added

--- controllers/user.js
+++ controllers/user.js
@@ -2,11 +2,34 @@ import userModel from "../Models/userModel.js";
 
 export const getUserLogs = async (req, res) => {
   const { id } = req.params;
+  const { from, to, limit } = req.query;
   console.log(id);
+
   try {
     const userLog = await userModel.findById(id);
     const { _id, username, count, log } = userLog;
-    res.status(200).json({ _id, username, count, log });
+
+    if (from === undefined && to === undefined) {
+      res.status(200).json({ _id, username, count, log });
+    } else {
+      let filtered;
+      if (!!from && !!to === false) {
+        filtered = log.filter((n) => new Date(n.date + "Z") >= new Date(from));
+      } else if (!!from === false && !!to) {
+        filtered = log.filter((n) => new Date(n.date + "Z") <= new Date(to));
+      } else if (!!from && !!to) {
+        filtered = log.filter(
+          (n) =>
+            new Date(from) >= new Date(n.date + "Z") &&
+            new Date(n.date + "Z") <= new Date(to)
+        );
+      }
+      const logLength =
+        !!limit && limit < filtered.length ? limit : filtered.length;
+
+      filtered = filtered.slice(0, logLength);
+      res.status(200).json({ _id, username, count: logLength, log: filtered });
+    }
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
